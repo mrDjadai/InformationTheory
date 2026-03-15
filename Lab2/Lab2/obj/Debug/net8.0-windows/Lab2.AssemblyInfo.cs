55d06bafda458167cc937b849775d95ce21914a8
Gamer Grapes <igor24122006@gmail.com>
2026-03-15 17:02:47 +0300

    AddedIgnore

--- Lab2/Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
+++ Lab2/Lab2/obj/Debug/net8.0-windows/Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+add82b3ac28839e1ee9c6a8e7a6beadd6c128e34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38380f85c64976e274bdd8a4e4425d4813ec4821")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
